81275cefe2231b5117fa0c02e972233d8fdbd768
yuvadany <toyuvaraj@gmail.com>
2021-03-11 16:13:39 +0100

    @CrossOrigin Annotation to provide access to other servers

--- src/main/java/com/purchase/home/controler/HomeController.java
+++ src/main/java/com/purchase/home/controler/HomeController.java
@@ -3,6 +3,7 @@ package com.purchase.home.controler;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,8 @@ import com.purchase.home.model.Items;
 import com.purchase.home.service.HomeService;
 
 @RestController
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 @RequestMapping("/v1/purchase")
 public class HomeController {
 
