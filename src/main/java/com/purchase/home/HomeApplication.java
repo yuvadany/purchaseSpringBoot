db82ef88e8f1ca82553dd77f47ba814c3569bc67
yuvadany <toyuvaraj@gmail.com>
2021-03-14 13:37:44 +0100

    logger slf4f initial change

--- src/main/java/com/purchase/home/HomeApplication.java
+++ src/main/java/com/purchase/home/HomeApplication.java
@@ -1,15 +1,17 @@
 package com.purchase.home;
 
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@SpringBootApplication  
+@SpringBootApplication
 public class HomeApplication {
+	static Logger logger = org.slf4j.LoggerFactory.getLogger(HomeApplication.class);
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeApplication.class, args);
-		System.out.println("Home Purchase App started...");
+		logger.info("Purchase Home App Started....");
 	}
 
 }
