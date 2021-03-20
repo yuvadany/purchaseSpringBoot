7483999f53e483d9bf71b2b9b676c806bc0d3d27
yuvadany <toyuvaraj@gmail.com>
2021-03-20 18:43:33 +0100

    spring Security and  logging from application.properties

--- src/main/java/com/purchase/home/HomeApplication.java
+++ src/main/java/com/purchase/home/HomeApplication.java
@@ -1,17 +1,17 @@
 package com.purchase.home;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HomeApplication {
-	static Logger logger = org.slf4j.LoggerFactory.getLogger(HomeApplication.class);
-
+	private static Logger logger = LoggerFactory.getLogger(HomeApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeApplication.class, args);
-		logger.info("Purchase Home App Started....");
+		logger.info("... Purchase - Home App Started ..");
 	}
 
 }
