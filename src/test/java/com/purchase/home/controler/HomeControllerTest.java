8173c5f13806b326d0f73d8af680dbe0b8260478
yuvadany <toyuvaraj@gmail.com>
2021-06-21 17:35:55 +0200

    sample for jenkins

---
+++ src/test/java/com/purchase/home/controler/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package com.purchase.home.controler;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HomeControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
